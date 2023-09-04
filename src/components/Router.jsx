3b0ad3a385e0c7e7e2e7073b0edbbca85c839cfa
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-09-04 11:06:55 +0400

    Add Home screen and Router

---
+++ src/components/Router.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./screens/Home/Home";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          element={<Home />}
+          path="/"
+        />
+
+        
+
+        <Route
+          element={<div>Page not found</div>}
+          path="*"
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/components/screens/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css"
+
+const Home = () => {
+  return (
+    <div>
+      <div className={styles.container}>Home</div>
+    </div>
+  );
+};
+
+export default Home;
