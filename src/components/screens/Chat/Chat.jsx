d3d81aec879525db09ce3fa6d2ff3ad473234e14
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-09-05 15:47:32 +0400

    Add ws connection

--- src/components/screens/Chat/Chat.jsx
+++ src/components/screens/Chat/Chat.jsx
@@ -1,9 +1,50 @@
 import { Link, useParams } from "react-router-dom"
 import styles from "./Chat.module.css"
+import { useRef, useState, useEffect } from "react";
+import { AuthService } from "../../../services/auth.service";
+
+
+
 
 
 function Chat() {
-  const {id}=useParams()
+  const { id } = useParams()
+  const [messages, setMessages] = useState([])
+  const [connected, setConnected] = useState(false)
+  const socket = useRef()
+  const cookieHeader = "token=" + localStorage.JWT + "; path=localhost:8008/";
+  document.cookie =cookieHeader
+
+  useEffect(() => {
+    socket.current = new WebSocket('ws://localhost:8008/chat')
+
+    socket.current.onopen = (e) => {
+      setConnected(true)
+      console.log("Connection open", e);
+    };
+
+    socket.current.onmessage = (e) => {
+      console.log("Connection open", e);
+    };
+
+    socket.current.onclose = (e) => {
+      console.log("Connection closed", e);
+    };
+
+    socket.current.onerror = (e) => {
+      console.log('Connection error',e);
+    }
+
+    return () => {
+      socket.current.close()
+      socket.current.onclose = (e) => {
+        console.log("close", e);
+      };
+
+  }
+  }, [])
+
+
   return (
     <div className={`container ${styles.chat}`}>
       <Link to="/topics">Назад</Link>
